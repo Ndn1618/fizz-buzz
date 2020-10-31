2b66085df6d608df3984eb6e93159cae93b3b5b8
Ndn1618 <ndilbar1618@gmail.com>
2020-10-31 23:16:50 +0500

    Add js function

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// Variables
+var DIVIDES_TO_THREE = 'Fizz';
+var DIVIDES_TO_FIVE = 'Buzz';
+var DIVIDES_TO_THREE_AND_FIVE = 'FizzBuzz';
+
+// Choosing elements
+var gameForm = document.querySelector('.game-form');
+var gameResult = document.querySelector('.game-result');
+
+gameForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  // Taking value of number input
+  var gameNumberInput = parseInt(gameForm.querySelector('.game-number-input').value.trim(), 10);
+
+  if (!isNaN(gameNumberInput)) {
+    if (gameNumberInput % 3 == 0 && gameNumberInput % 5 == 0) {
+      gameResult.textContent = DIVIDES_TO_THREE_AND_FIVE;
+    } else if (gameNumberInput % 3 == 0) {
+      gameResult.textContent = DIVIDES_TO_THREE;
+    }
+    else if (gameNumberInput % 5 == 0) {
+      gameResult.textContent = DIVIDES_TO_FIVE;
+    }
+    else {
+      gameResult.textContent = gameNumberInput;
+    }
+  } else {
+      gameResult.textContent = 'Iltimos, son kiriting )';
+  }
+});
